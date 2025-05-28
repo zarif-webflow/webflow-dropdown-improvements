83b4cc9966c7c699dc8bb31282c36a80c9bd0775
zarif-webflow <zarif.webflow@gmail.com>
2025-05-28 22:53:04 +0600

    feat: add proper margin functionality

--- src/features/dropdown-improvements.ts
+++ src/features/dropdown-improvements.ts
@@ -1,4 +1,14 @@
+import { setStyle } from '@/utils/util';
+import { autoUpdate, computePosition, flip, shift, size } from '@floating-ui/dom';
+
 const initDropdownImprovements = () => {
+  const DEFAULT_MARGIN = '0.5rem';
+
+  const windowSpecifiedMargin =
+    'globalDropdownMargin' in window && typeof window.globalDropdownMargin === 'string'
+      ? window.globalDropdownMargin
+      : DEFAULT_MARGIN;
+
   const webflowDropdownElements = Array.from(document.querySelectorAll<HTMLElement>('.w-dropdown'));
 
   for (const dropdownElement of webflowDropdownElements) {
@@ -14,6 +24,35 @@ const initDropdownImprovements = () => {
       console.warn('Dropdown list not found for:', dropdownElement);
       continue;
     }
+
+    const dropdownMargin =
+      dropdownElement.getAttribute('data-dropdown-margin') ||
+      windowSpecifiedMargin ||
+      DEFAULT_MARGIN;
+
+    const setModalPosition = () => {
+      computePosition(dropdownToggle, dropdownList, {
+        placement: 'bottom-start',
+        middleware: [
+          flip(),
+          shift(),
+          size({
+            apply: ({ rects }) => {
+              setStyle(dropdownList, { 'min-width': `${rects.reference.width}px` });
+            },
+          }),
+        ],
+      }).then(({ y, placement }) => {
+        setStyle(dropdownList, {
+          top: `${y}px`,
+          transform: placement.includes('bottom')
+            ? `translateY(${dropdownMargin})`
+            : `translateY(calc(-1 * ${dropdownMargin}))`,
+        });
+      });
+    };
+
+    autoUpdate(dropdownToggle, dropdownList, setModalPosition);
 
     console.log(dropdownList);
   }
